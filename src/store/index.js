7e3cf07788eddefdee5d4644478d89d1e0e92e05
missionctrlbiz <teewhy_007@yahoo.com>
2024-05-24 20:48:05 +0100

    VS

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import imageSlice from '../store/counter/ImageSlice'
-
-
-export const store = configureStore({
-  reducer: {
-    image: imageSlice
-  },
-})
