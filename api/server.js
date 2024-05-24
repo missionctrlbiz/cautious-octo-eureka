96c034838126c15cda54dce19dce843f66e74d2b
missionctrlbiz <teewhy_007@yahoo.com>
2024-05-24 22:46:15 +0100

    Updated configuration for Vercel

--- api/server.js
+++ api/server.js
@@ -5,11 +5,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // CORS Configuration
 const corsOptions = {
-    origin: 'https://silencecoderr-portfolio.vercel.app',
+    origin: 'https://silencecoderr-portfolio.vercel.app', 
     methods: ['GET', 'POST'],
     credentials: true,
     optionsSuccessStatus: 200
@@ -18,7 +17,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-// Endpoint to fetch portfolio data
 app.get('/api/portfolio', async (req, res) => {
     try {
         const response = await axios.get('https://api.jsonbin.io/v3/b/664f9679ad19ca34f86e10bc', {
@@ -27,7 +25,10 @@ app.get('/api/portfolio', async (req, res) => {
                 'X-MASTER-KEY': '$2a$10$hGEua.fk2zjUH4ho80nmcuTDtW5dGXptKgrifYfbS9SwjJTfxoJ6K'
             }
         });
-
+        res.setHeader('Access-Control-Allow-Origin', 'https://silencecoderr-portfolio.vercel.app');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         res.json(response.data);
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -38,7 +39,6 @@ app.get('/api/portfolio', async (req, res) => {
 app.post('/api/send-email', async (req, res) => {
     const { fullName, email, phone, subject, message } = req.body;
 
-    // Create a transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
@@ -47,7 +47,6 @@ app.post('/api/send-email', async (req, res) => {
         }
     });
 
-    // Define the email options
     const mailOptions = {
         from: email,
         to: 'recipient-email@example.com',
@@ -55,7 +54,6 @@ app.post('/api/send-email', async (req, res) => {
         text: `You have a new message from ${fullName} (${phone}):\n\n${message}`
     };
 
-    // Send the email
     try {
         await transporter.sendMail(mailOptions);
         res.status(200).send({ message: 'Email sent successfully!' });
@@ -66,6 +64,7 @@ app.post('/api/send-email', async (req, res) => {
 });
 
 // Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
